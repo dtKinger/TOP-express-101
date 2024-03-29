c4b14871c30faca9af5fd4cc9859dc540af0fa77
Dan King <king.daniel.thomas@gmail.com>
2024-03-29 12:59:24 +0700

    Add skeleton routes and configure landing page route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog')
 
 var app = express();
 
@@ -53,7 +54,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/catalog', catalogRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const asyncHandler = require("express-async-handler");
-const Books = require('../models/book')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', links: [{"page": 'Contact', url: '/contact-us'}, {page: "About Us", url: "/about"}]});
+  // res.render('index', { title: 'Express', links: [{"page": 'Contact', url: '/contact-us'}, { page: "About Us", url: "/about" }, { page: "Catalog", url: "/catalog"} ]});
+  res.redirect('/catalog')
 });
 
-router.get( "/catalog", asyncHandler(async (req, res, next) => {
-    const successfulResult = await Books.find({}).exec();
-    res.render("catalog", { title: "All Books", list: successfulResult });
-  }),
-);
-
 // Playing with req.params and URL parameters
-router.get('/:user_id/*/:name', function (req, res, next) {
-  res.send(req.params)
-})
+// router.get('/:user_id/*/:name', function (req, res, next) {
+//   res.send(req.params)
+// })
 
 router.get('/about', (req, res, next) => {
   res.render('index', {title: 'About Us'})
